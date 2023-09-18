ea503eebc12e917513565d1b5b9c89d531190c7f
radwaahmed97 <radwa.ahmed95@yahoo.com>
2023-09-18 13:49:24 -0400

    params.c

---
+++ params.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * init_params - let struct field cleared and buf reset.
+ * @params: parameters struct
+ * @ap: pointer of arguments
+ * Return: void
+ */
+
+void init_params(parameters *params, va_list ap)
+{
+	params->unsign = 0;
+
+	params->plus = 0;
+	params->space = 0;
+	params->hashtag = 0;
+	params->zero = 0;
+	params->minus = 0;
+
+	params->width = 0;
+	params->percision = UNIT_MAX;
+
+	params->h = 0;
+	params->l = 0;
+	void(ap);
+}
+
